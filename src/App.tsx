5411b5cee345704f2f16915439705c32dcd89148
CozyBrian <dragonbrian33@gmail.com>
2023-09-14 05:05:59 +0000

    initial commit lol

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import ParticlesView from "./particles"
+
+function App() {
+
+  return (
+    <>
+    <main className='flex items-center justify-center h-screen backdrop-blur-[96px]'>
+      <h1 className="text-white text-5xl">Hello</h1> 
+    </main>
+    <ParticlesView />
+    </>
+  )
+}
+
+export default App
---
+++ src/particles/constants.ts
@@ -0,0 +1,74 @@
+import type {
+  IOptions,
+  RecursivePartial,
+} from "tsparticles-engine";
+
+export const particlesOptions: RecursivePartial<IOptions> = {
+  fullScreen: {
+    enable: true,
+    zIndex: -1,
+  },
+  background: {
+    color: {
+      value: "#0f172a",
+    },
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onHover: {
+        enable: false,
+        mode: "repulse",
+      },
+      resize: false,
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#38bdf8",
+    },
+    links: {
+      color: "#bae6fd",
+      distance: 75,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: false,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "out",
+      },
+      random: false,
+      speed: 0.75,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 120,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 2 },
+    },
+  },
+  detectRetina: true,
+};
